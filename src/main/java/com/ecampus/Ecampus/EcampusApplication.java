fff553a061f5ce9a553f2173a0a940f4f3d4775c
crystisor <crystisor4@gmail.com>
2024-12-09 12:02:59 +0200

    dewfw

---
+++ src/main/java/com/ecampus/Ecampus/EcampusApplication.java
@@ -0,0 +1,13 @@
+package com.ecampus.Ecampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcampusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecampus/Ecampus/EcampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecampus.Ecampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

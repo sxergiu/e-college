187fdd0b037e0bb932069af4afc156466ec6b030
charlie <sxergiu@gmail.com>
2025-01-08 14:17:13 +0200

    no longer displaying chatid

--- Frontend/e-college/src/components/chat/index.jsx
+++ Frontend/e-college/src/components/chat/index.jsx
@@ -160,7 +160,7 @@ const ChatComponent = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                   <path fillRule="evenodd" d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clipRule="evenodd" />
                 </svg>
-                <h2 className="font-semibold">Chat {selectedChatId}</h2>
+                <h2 className="font-semibold">Chat with</h2>
               </div>
               <div className="flex items-center text-sm text-gray-500 mt-1">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" viewBox="0 0 20 20" fill="currentColor">
